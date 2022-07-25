1b9cc9dcab6363e3be7ae9041ea78cc43575ed07
GermanHv <ghove7@gmail.com>
2022-07-25 18:54:37 -0500

    3. variables

---
+++ JS-basico/variables.js
@@ -0,0 +1,21 @@
+var nombre= "German";
+
+// declarar variable
+var edad;
+// inicializar
+edad = 34;
+
+edad;
+
+//array
+var elementos = ["lapiz","coche","hola"];
+
+//objeto
+var usuario={
+    nombre: "German",
+    edad: 13
+}
+// mando a llamar la variable
+usuario;
+
+console.log(elementos[2]); // consola es de igual forma una variable que le pertenece al navegador, con cierto espacio en memoria, y con un tipo de comportamiento especial. Donde Log es un método de la función
