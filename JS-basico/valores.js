2767a16e86c10245e740fd4053ac4706c283fbcc
GermanHv <ghove7@gmail.com>
2022-07-25 17:24:54 -0500

    2. Valores

---
+++ JS-basico/valores.js
@@ -0,0 +1,24 @@
+
+23 // tipo numero
+
+Hola esto no es un Texto // No lo entiende JS
+
+"Hola esto SI es un texto" //string
+
+/* Valores booleanos */
+true
+false 
+ // Sirven para validar.
+
+ /* Valores vacios o placeholder */
+ null
+ undefined
+ // genera un espacio reservado para algun valor faltante. evitar undefined.
+
+ /*  Valores no primitivos o tipo objetos */
+
+ //array 
+ [5,3,2,1]
+
+ // objeto
+ {name: "German"}
