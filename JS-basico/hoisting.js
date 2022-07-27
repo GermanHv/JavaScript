d694d01047df58fbfea6fe116c5c19edbfd3e5cb
GermanHv <ghove7@gmail.com>
2022-07-27 17:38:03 -0500

    6. hoisting, readme

---
+++ JS-basico/hoisting.js
@@ -0,0 +1,24 @@
+// Las variables y funciones se procesan antes de el codigo
+
+console.log(Planeta);
+
+
+
+
+Hola();
+
+function Hola(){
+    console.log ("Hola"+ Planeta);
+}
+
+var Planeta = "Tierra"; // Inicializo variable
+
+
+// ---------- Hoisting en variable 
+
+var miNombre = undefined;
+
+console.log(miNombre + "Hoisting")
+
+miNombre = "Vx"
+
---
+++ JS-basico/readme.md
@@ -0,0 +1,8 @@
+# Curso de JS Básico
+
+Curso: [JS Básico](https://platzi.com/cursos/basico-javascript/)
+
+
+
+Repositorio: [degranda jsBasico](https://github.com/degranda/jsBasico-)
+ 
