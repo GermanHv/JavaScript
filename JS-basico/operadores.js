12a7cf893a618743aec774e7e78ac87d3c364672
GermanHv <ghove7@gmail.com>
2022-07-27 20:01:08 -0500

    9. operadores

---
+++ JS-basico/operadores.js
@@ -0,0 +1,38 @@
+// operadores comunes aritmeticos
+3+2
+59-9
+13*2
+9/3
+
+// Concatenacion, los junta en uno. en string
+"Planeta" + " Tierra"
+
+!false // ! (not) unit operator , negacion. En este caso es True
+
+//Asignacion
+var a=1;
+
+// comparacion
+
+3=="3"; // true, 2 iguales compara el valor no el tipo de valor.
+3 === "3"; // false, valida que los dos tipos y valores sean exactamente iguales, un dato es numero y el otro string
+
+5<3; // false
+
+4>3;
+
+5<=6
+5>=6
+
+// Si alguno de los dos es falso no se cumple la condicion
+a && b
+
+// Or
+a || b 
+
+//
+
+var edad = 40
+edad++ // incrementa por uno
+
+edad += 2 // incrementa por dos
